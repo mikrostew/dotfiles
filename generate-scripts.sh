ec8d92d1fa0ad7356f5144db1d42505a50317a6b
Michael Stewart <mikrostew@gmail.com>
2018-11-29 15:39:09 -0800

    use checksums to skip files if they don't need to be generated

--- generate-scripts.sh
+++ generate-scripts.sh
@@ -4,8 +4,12 @@
 
 COLOR_RESET='\033[0m'
 COLOR_FG_RED='\033[0;31m'
+COLOR_FG_YELLOW='\033[0;33m'
 
 declare -A sourced_files=()
+
+files_generated=0
+files_skipped=0
 
 # echo to stderr with red text
 echo_err() {
@@ -22,15 +26,62 @@ on_error() {
 
 trap on_error ERR
 
+# compare hashes to check if the file needs to be generated
+need_to_generate() {
+  # arguments:
+  local src_script="$1"
+  local generated_script="$2"
+
+  # if the generated file doesn't exist, then of course it should be generated
+  if [ ! -f "$generated_script" ]
+  then
+    return 0
+  fi
+
+  # get hashfrom last file generation
+  readarray -t -s5 -n1 hash_lines < "$generated_script"
+  # remove the leading hash
+  local prev_hash="${hash_lines[0]#\#}"
+
+  # for the generated script, skip the header (which contains the calculated hash) and is currently 7 lines long
+  local combined_hash="$(hash_for_scripts "$src_script" "$(tail -n +8 $generated_script)")"
+
+  # if hash matches, no need to regenerate
+  if [ "$combined_hash" == "$prev_hash" ]
+  then
+    return 1
+  fi
+
+  # otherwise have to generate
+  return 0
+}
+
+# calculate the hash of the src and dest scripts
+hash_for_scripts() {
+  # arguments:
+  local src_file="$1"
+  local generated_contents="$2"
+
+  # combine files and calculate the hash (instead of doing 2 separate hashes)
+  # - using md5 because I want this to be fast and I'm not worried about collisions
+  echo "$generated_contents" | cat "$src_file" - | md5 -q
+}
+
 # create a header for the generated script file
 file_header() {
-  input_file="$1"
-  file_contents="$2"
+  # arguments:
+  local input_file="$1"
+  local file_contents="$2"
+
+  # calculate the hash
+  local combined_hash="$(hash_for_scripts "$input_file" "$file_contents")"
+
   echo "#!/usr/bin/env bash"
   echo "###########################################################################"
   echo "# DO NOT EDIT! This script was auto-generated. To update this script, edit"
   echo "# the file $input_file, and run './generate-scripts.sh'"
   echo "###########################################################################"
+  echo "#$combined_hash"
   echo ""
   echo "$file_contents"
 }
@@ -229,130 +280,143 @@ do
   # this is where the file will be output
   new_file_name="${script_file/script-gen/scripts}"
 
-  # read file contents
-  file_contents=$(<"$script_file")
-
-  # variable and function imports
-  declare -A var_imports
-  declare -A var_import_sources
-  declare -A func_imports
-  declare -A func_import_sources
-  declare -A cmd_requirements
-
-  # lines from the input script that are not imports
-  other_lines=()
-  # keep track of the current line number for errors
-  current_line=0
-  while IFS= read -r line
-  do
-    current_line=$(( current_line + 1 ))
-
-    if [[ "$line" =~ ^@import_var\ {\ (.*)\ }\ from\ (.*)$ ]]
-    then
-      # @import { MULTIPLE, VARIABLES } from file
-      var_names="${BASH_REMATCH[1]}"
-      file_name="${BASH_REMATCH[2]}"
-      import_multiple_variables "$var_names" "$file_name"
-    elif [[ "$line" =~ ^@import_var\ (.*)\ from\ (.*)$ ]]
-    then
-      # @import_var VARIABLE from file
-      var_name="${BASH_REMATCH[1]}"
-      file_name="${BASH_REMATCH[2]}"
-      var_value="${!var_name}"
-      import_variable "$var_name" "$var_value" "$file_name"
-    elif [[ "$line" =~ ^@import\ {\ (.*)\ }\ from\ (.*)$ ]]
-    then
-      # @import { multiple, functions } from file
-      func_names="${BASH_REMATCH[1]}"
-      file_name="${BASH_REMATCH[2]}"
-      import_multiple_functions "$func_names" "$file_name"
-    elif [[ "$line" =~ ^@import\ (.*)\ from\ (.*)$ ]]
-    then
-      # @import function from file
-      func_name="${BASH_REMATCH[1]}"
-      file_name="${BASH_REMATCH[2]}"
-      import_function "$func_name" "$file_name"
-    elif [[ "$line" =~ ^@uses_cmds\ (.*)$ ]]
-    then
-      cmd_names="${BASH_REMATCH[1]}"
-      add_cmd_requirements "$cmd_names"
-    elif [[ "$line" =~ \?PLATFORM_IS_MAC\? ]]
-    then
-      replace_test="$(echo "$line" | sed 's/\?PLATFORM_IS_MAC\?/[ "$(uname -s)" == "Darwin" ]/')"
-      other_lines+=( "$replace_test" )
-    elif [[ "$line" =~ \?PLATFORM_IS_LINUX\? ]]
-    then
-      replace_test="$(echo "$line" | sed 's/\?PLATFORM_IS_LINUX\?/[ "$(uname -s)" == "Linux" ]/')"
-      other_lines+=( "$replace_test" )
-    elif [[ "$line" =~ ^(\ *)@EXIT_IF_CMD_ERROR\ \"(.*)\"$ ]]
-    then
-      # TODO: allow specifying functions to call before exiting
-      padding="${BASH_REMATCH[1]}"
-      exit_msg="${BASH_REMATCH[2]}"
-      exit_with_message gen_code "$exit_msg" "$padding"
-      other_lines+=( "$gen_code" )
-    # TODO: option to generate help docs
-    else
-      # no import
-      other_lines+=( "$line" )
-    fi
-  done <<< "$file_contents"
-
-  # build variable and function imports
-  var_import_lines=()
-  var_keys="$(for key in "${!var_imports[@]}"; do echo "$key"; done | sort | tr '\n' ' ')" # sort keys
-  for var_name in $var_keys
-  do
-    var_import_lines+=( "$var_name='${var_imports[$var_name]}'" ) # single quotes around value
-  done
-  func_import_lines=()
-  func_keys="$(for key in "${!func_imports[@]}"; do echo "$key"; done | sort | tr '\n' ' ')" # sort keys
-  for func_name in $func_keys
-  do
-    func_import_lines+=( "${func_imports[$func_name]}" )
-  done
-
-  # build cmd requirement checking
-  cmd_requirement_lines=()
-  if [ "${#cmd_requirements[@]}" -gt 0 ]
+  if need_to_generate "$script_file" "$new_file_name"
   then
-    # add the function the check command requirements
-    func_import_lines+=( "$(cmd_requirement_function)" )
-    cmd_requirement_lines+=( 'combined_return=0' )
-    for cmd_name in "${!cmd_requirements[@]}"
+    # read file contents
+    file_contents=$(<"$script_file")
+
+    # variable and function imports
+    declare -A var_imports
+    declare -A var_import_sources
+    declare -A func_imports
+    declare -A func_import_sources
+    declare -A cmd_requirements
+
+    # lines from the input script that are not imports
+    other_lines=()
+    # keep track of the current line number for errors
+    current_line=0
+    while IFS= read -r line
     do
-      how_to_install="${cmd_requirements[$cmd_name]}"
-      cmd_requirement_lines+=( "requirement_check $cmd_name \"$how_to_install\"" )
-      cmd_requirement_lines+=( 'combined_return=$(( combined_return + $? ))' )
+      current_line=$(( current_line + 1 ))
+
+      if [[ "$line" =~ ^@import_var\ {\ (.*)\ }\ from\ (.*)$ ]]
+      then
+        # @import { MULTIPLE, VARIABLES } from file
+        var_names="${BASH_REMATCH[1]}"
+        file_name="${BASH_REMATCH[2]}"
+        import_multiple_variables "$var_names" "$file_name"
+      elif [[ "$line" =~ ^@import_var\ (.*)\ from\ (.*)$ ]]
+      then
+        # @import_var VARIABLE from file
+        var_name="${BASH_REMATCH[1]}"
+        file_name="${BASH_REMATCH[2]}"
+        var_value="${!var_name}"
+        import_variable "$var_name" "$var_value" "$file_name"
+      elif [[ "$line" =~ ^@import\ {\ (.*)\ }\ from\ (.*)$ ]]
+      then
+        # @import { multiple, functions } from file
+        func_names="${BASH_REMATCH[1]}"
+        file_name="${BASH_REMATCH[2]}"
+        import_multiple_functions "$func_names" "$file_name"
+      elif [[ "$line" =~ ^@import\ (.*)\ from\ (.*)$ ]]
+      then
+        # @import function from file
+        func_name="${BASH_REMATCH[1]}"
+        file_name="${BASH_REMATCH[2]}"
+        import_function "$func_name" "$file_name"
+      elif [[ "$line" =~ ^@uses_cmds\ (.*)$ ]]
+      then
+        cmd_names="${BASH_REMATCH[1]}"
+        add_cmd_requirements "$cmd_names"
+      elif [[ "$line" =~ \?PLATFORM_IS_MAC\? ]]
+      then
+        replace_test="$(echo "$line" | sed 's/\?PLATFORM_IS_MAC\?/[ "$(uname -s)" == "Darwin" ]/')"
+        other_lines+=( "$replace_test" )
+      elif [[ "$line" =~ \?PLATFORM_IS_LINUX\? ]]
+      then
+        replace_test="$(echo "$line" | sed 's/\?PLATFORM_IS_LINUX\?/[ "$(uname -s)" == "Linux" ]/')"
+        other_lines+=( "$replace_test" )
+      elif [[ "$line" =~ ^(\ *)@EXIT_IF_CMD_ERROR\ \"(.*)\"$ ]]
+      then
+        # TODO: allow specifying functions to call before exiting
+        padding="${BASH_REMATCH[1]}"
+        exit_msg="${BASH_REMATCH[2]}"
+        exit_with_message gen_code "$exit_msg" "$padding"
+        other_lines+=( "$gen_code" )
+      # TODO: option to generate help docs
+      else
+        # no import
+        other_lines+=( "$line" )
+      fi
+    done <<< "$file_contents"
+
+    # build variable and function imports
+    var_import_lines=()
+    var_keys="$(for key in "${!var_imports[@]}"; do echo "$key"; done | sort | tr '\n' ' ')" # sort keys
+    for var_name in $var_keys
+    do
+      var_import_lines+=( "$var_name='${var_imports[$var_name]}'" ) # single quotes around value
     done
-    cmd_requirement_lines+=( 'if [ "$combined_return" != 0 ]; then exit $combined_return; fi' )
+    func_import_lines=()
+    func_keys="$(for key in "${!func_imports[@]}"; do echo "$key"; done | sort | tr '\n' ' ')" # sort keys
+    for func_name in $func_keys
+    do
+      func_import_lines+=( "${func_imports[$func_name]}" )
+    done
+
+    # build cmd requirement checking
+    cmd_requirement_lines=()
+    if [ "${#cmd_requirements[@]}" -gt 0 ]
+    then
+      # add the function the check command requirements
+      func_import_lines+=( "$(cmd_requirement_function)" )
+      cmd_requirement_lines+=( 'combined_return=0' )
+      for cmd_name in "${!cmd_requirements[@]}"
+      do
+        how_to_install="${cmd_requirements[$cmd_name]}"
+        cmd_requirement_lines+=( "requirement_check $cmd_name \"$how_to_install\"" )
+        cmd_requirement_lines+=( 'combined_return=$(( combined_return + $? ))' )
+      done
+      cmd_requirement_lines+=( 'if [ "$combined_return" != 0 ]; then exit $combined_return; fi' )
+    fi
+
+    # join imports and other lines (joined with newlines)
+    with_imports="$(
+      IFS=$'\n';
+      echo "${var_import_lines[*]}";
+      echo "${func_import_lines[*]}";
+      echo "${cmd_requirement_lines[*]}";
+      echo "${other_lines[*]}";
+    )"
+
+    # strip any comments and blank lines
+    no_comments="$(echo "$with_imports" | sed '/^[[:blank:]]*#/d;/^[[:blank:]]*$/d;')"
+
+    # add a header and shebang to the beginning
+    with_header="$(file_header "$script_file" "$no_comments")"
+
+    # write the new file
+    echo "$script_file -> $new_file_name"
+    echo "$with_header" > "$new_file_name"
+    files_generated=$((files_generated+1))
+
+    # clear arrays
+    unset var_imports
+    unset var_import_sources
+    unset func_imports
+    unset func_import_sources
+    unset cmd_requirements
+  else
+    # don't need to generate file
+    echo -e "${COLOR_FG_YELLOW}$script_file -> $new_file_name (skipped)${COLOR_RESET}"
+    files_skipped=$((files_skipped+1))
   fi
 
-  # join imports and other lines (joined with newlines)
-  with_imports="$(
-    IFS=$'\n';
-    echo "${var_import_lines[*]}";
-    echo "${func_import_lines[*]}";
-    echo "${cmd_requirement_lines[*]}";
-    echo "${other_lines[*]}";
-  )"
-
-  # strip any comments and blank lines
-  no_comments="$(echo "$with_imports" | sed '/^[[:blank:]]*#/d;/^[[:blank:]]*$/d;')"
-
-  # add a header and shebang to the beginning
-  with_header="$(file_header "$script_file" "$no_comments")"
-
-  # write the file, ensuring that it has executable bits set
-  echo "$script_file -> $new_file_name"
-  echo "$with_header" > "$new_file_name"
+  # make sure the generated script is executable
   chmod +x "$new_file_name"
-
-  # clear arrays
-  unset var_imports
-  unset var_import_sources
-  unset func_imports
-  unset func_import_sources
-  unset cmd_requirements
 done
+
+echo ""
+echo "generated $files_generated, skipped $files_skipped"
 
